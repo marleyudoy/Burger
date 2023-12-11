b2259d66084127ab0be8080997d710212469da13
marleyudoy <umudoy364@gmail.com>
2023-12-11 10:59:05 +0600

    this is my burger shop web page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    screens:{
+      "sm" : "480px",
+      "md" : "768px",
+      "lg" : "1024px"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
